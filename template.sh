ab5041ec3ffc9c38edd27642727f03762cfdf49c
Ceernam Jayanth Kumar <ceernamjayanth@gmail.com>
2025-08-02 02:30:12 +0530

    Add all project File

---
+++ template.sh
@@ -0,0 +1,18 @@
+# With the help of bash script i Have created my 
+
+# creating Directorys
+mkdir -p src
+mkdir -p research
+#  Creating Files 
+touch src/__init__.py
+touch src/helper.py
+touch src/promt.py
+touch .env
+touch setup.py
+touch research/trials.ipynb
+touch requirements.txt
+
+# Echo linux comand is used to give the message 
+Echo "Directory and files created successfully !."
+
+
